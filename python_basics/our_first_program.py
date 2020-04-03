6a8a0b1c92494b63dd44ea71a9f8ab1565332a84
Miles Eastwood <milotheengineer@gmail.com>
2020-04-03 16:33:50 +0100

    Added file for if statements

---
+++ python_basics/our_first_program.py
@@ -0,0 +1,11 @@
+# This progrom says hello and asks for my name
+
+print("Hello World")
+print ("What is your name?") # ask for their name
+myName = input()
+print ("In is nice to meet you, " + myName)
+print("The length of your name is: ")
+print(len(myName))
+print("WHat is your age?") # ask for age
+myAge = input()
+print("You will be " + str(int(myAge) + 1) + " in a years time")
